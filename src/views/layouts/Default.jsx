5c81b2d4003576e8d10216b30453289ab83d658b
Naufal_abdullah <lnaufa90@gmail.com>
2023-09-21 22:41:57 +0700

    a wierd changes

--- src/views/layouts/Default.jsx
+++ src/views/layouts/Default.jsx
@@ -40,8 +40,14 @@ const Default = () => {
       </nav>
 
       <div className='flex'>
-        {sidebar ?<div>sidebar</div> : <div>s</div>}
-        {/* <Outlet /> */}
+        {sidebar ?<div>sidebar</div> : 
+        <div className=' bg-slate-800 w-14 border-r m-1'>
+          <div className='py-5 w-full flex justify-center bg-neutral-500'>
+          <img className='' src="./assets/hamburg-002.svg" alt="" />
+          {/* <p className='text-xs'>home</p> */}
+          </div>
+        </div>}
+        <Outlet />
       </div>
     </>
   )
